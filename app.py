128aead3c1aebf1db2dcb7c9103d8f295b2e8d24
MJ <mellejorna@gmail.com>
2024-03-27 12:12:53 +0100

    test

--- app.py
+++ app.py
@@ -20,6 +20,43 @@ if uploaded_files is not None:
     st.write(f"Bestand 1: {file1.getDocumentInfo().title}\nBestand 2: {file2.getDocumentInfo().title}")
     st.write("Voer de query uit, en de tool zal de twee bestanden vergelijken.")
 
+    text1 = ""
+    for page in file1.pages:
+        text1 += CharacterTextSplitter.split(page.extractText()) + "\n"
+
+    text2 = ""
+    for page in file2.pages:
+        text2 += CharacterTextSplitter.split(page.extractText()) + "\n"
 
 
+def process_document(uploaded_files, user_question):
+    document1_text = text1
+    document2_text = text2
+    
+    template = """
 
+    Jij bent een expert boekhouder en accountant. Je beantwoord de {user_question} over de volgende documenten: {document1_text} en {document2_text}.
+    Hierbij stel je nauwkeurigheid en volledigheid op prioriteit nummer 1. 
+    
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+
+    llm = ChatOpenAI(api_key=st.secrets["OPENAI_API_KEY"], model = "gpt-4-0125-preview", temperature=0, streaming=True)
+    chain = prompt | llm | StrOutputParser()
+    return chain.stream({
+        "document_text": document1_text + document2_text,
+        "user_question": user_question
+    })
+
+
+def main():
+    st.title("Courant Checker")
+    selected_document = uploaded_files(file1, file2)
+    if selected_document:
+        user_question = st.text_input("Stel een vraag over de polisvoorwaarden:")
+        if user_question:
+            document_stream = process_document(selected_document['path'], user_question)
+            st.write_stream(document_stream)  
+
+if __name__ == "__main__":
+    main()

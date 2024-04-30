325a3140790c8cd8af433846b52b90f248cab32b
MelleJo <mellejorna@gmail.com>
2024-04-30 14:13:55 +0400

    tst

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage
-from langgraph.graph import END, MessageGraph
+from langchain.graph import END, MessageGraph
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.prompts import PromptTemplate
@@ -16,12 +16,13 @@ openai_api_key = st.secrets["OPENAI_API_KEY"]
 groq_api_key = st.secrets["GROQ_API_KEY"]
 
 GROQ_LLM = ChatGroq(
-            model="llama3-70b-8192", api_key= groq_api_key, temperature = 0
-        )
+    model="llama3-70b-8192", api_key=groq_api_key, temperature=0
+)
 
 st.title("COURANT CHECKER")
 doc_1 = st.file_uploader("Upload Document 1", type="pdf")
 doc_2 = st.file_uploader("Upload Document 2", type="pdf")
+
 
 def extract_pdf_text(pdf_file1, pdf_file2):
     reader1 = PdfReader(pdf_file1)
@@ -29,11 +30,10 @@ def extract_pdf_text(pdf_file1, pdf_file2):
     text_doc1 = ""
     text_doc2 = ""
     for page in reader1.pages:
-        text_doc1 += page.extract_text()
+        text_doc1 += page.extractText()
     for page in reader2.pages:
-        text_doc2 += page.extract_text()
+        text_doc2 += page.extractText()
     return text_doc1, text_doc2
-
 
 
 # text -> tabel
@@ -43,6 +43,7 @@ prompt = PromptTemplate(
     Je gebruikt daarvoor {text_doc1} en {text_doc2}, je geeft duidelijk aan bij elke transactie van welk document het afkomstig is. 
 
     """,
+)
 
 extract_structure_agent = prompt | GROQ_LLM | StrOutputParser()
 
@@ -50,6 +51,4 @@ text_doc1, text_doc2 = extract_pdf_text(doc_1, doc_2)
 
 result = extract_structure_agent.invoke({"text_doc1": text_doc1, "text_doc2": text_doc2})
 
-print(result)
-
-st.print(result)
+st.write(result)

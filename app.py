2bd16335d1e91ea5f21493938a853c84105a3047
MelleJo <mellejorna@gmail.com>
2024-05-02 12:45:58 +0400

    test

--- app.py
+++ app.py
@@ -5,3 +5,16 @@ from langgraph.graph import END, MessageGraph
 
 st.title("Courantchecker")
 openai_api_key = st.secrets["OPENAI_API_KEY"]
+
+model = ChatOpenAI(temperature=0)
+
+graph = MessageGraph()
+
+graph.add_node("oracle", model)
+graph.add_edge("oracle", END)
+
+graph.set_entry_point("oracle")
+
+runnable = graph.compile()
+
+st.write(runnable.invoke(HumanMessage("What is 1 + 1?")))
